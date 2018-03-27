b6bb6291591fed94e30bfd1beb58024fc6e64bbf
Hoshi Chen <hchen21@albany.edu>
2018-03-27 18:55:21 -0400

    Testing opeing a file in OpenCV

--- ProjectYelp.py
+++ ProjectYelp.py
@@ -1,10 +1,22 @@
 # Libraries used
+import os
+import numpy as np
+import cv2
+import matplotlib as plt
+
+# -----------------------Loading the photos-----------------------
+# Changing the directory to the photo folder
+os.chdir("yelp_photos")
+os.chdir("photos")
 
 
-# Loading the photos
+img = cv2.imread("___3eKRa-uA97sdEZTJ9rQ.jpg", 0)
+
+cv2.imshow('image',img)
+cv2.waitKey(0)
 
 
-# Extracting the photos
+# ---------------------Extracting the photos---------------------
 
 
 # Building a classification model with ___
